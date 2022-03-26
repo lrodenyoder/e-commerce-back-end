85733294d4f054fc433c863277c1f956f3947f69
lrodenyoder <rodenlau000@gmail.com>
2022-03-26 10:33:43 -0500

    category model setup

--- server.js
+++ server.js
@@ -14,6 +14,4 @@ app.use(routes);
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log("Now listening"))
 });
-// app.listen(PORT, () => {
-//   console.log(`App listening on port ${PORT}!`);
-// });
+
